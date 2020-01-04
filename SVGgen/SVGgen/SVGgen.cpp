d4c356d1f9377562e368a0a6eb7deb23c317797f
TrigerxD <trigerxd@gmail.com>
2020-01-04 15:47:46 +0100

    added base window showing, class structure for UI and Control to manage data flow betweend message handle and view struct

--- SVGgen/SVGgen/SVGgen.cpp
+++ SVGgen/SVGgen/SVGgen.cpp
@@ -1,7 +1,145 @@
 #include "SVGgen.h"
 
-int main()
+UI * UI::me = NULL;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
 {
-    std::cout << "Hello World!\n"; 
+
+	MSG komunikat;
+	UI okno = UI(hInstance, (LPSTR)"Klasa Okienka", 640, 480);
+
+	if (RegisterClassEx(&okno.getWindow())) {
+		okno.createView(hInstance, (LPSTR)"SVGgen");
+		if (okno.getView()) {
+			ShowWindow(okno.getView(), nCmdShow);
+			UpdateWindow(okno.getView());
+
+			while (GetMessage(&komunikat, NULL, 0, 0)) {
+				//Pêtla zdarzeñ
+				TranslateMessage(&komunikat);
+				DispatchMessage(&komunikat);
+			}
+		}
+	}
+	return komunikat.wParam;
 }
 
+UI::UI() 
+{
+	generate = Control();
+	show = Control();
+	me = this;
+}
+
+UI::UI(HINSTANCE instance, LPSTR className, int width, int height) 
+{
+	UI();
+	this->width = width;
+	this->height = height;
+	this->className = className;
+	window.cbSize = sizeof(WNDCLASSEX);
+	window.style = 0;
+	window.lpfnWndProc = Proc;
+	window.cbClsExtra = 0;
+	window.cbWndExtra = 0;
+	window.hInstance = instance;
+	window.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	window.hCursor = LoadCursor(NULL, IDC_ARROW);
+	window.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	window.lpszMenuName = NULL;
+	window.lpszClassName = className;
+	window.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+}
+
+WNDCLASSEX UI::getWindow()
+{
+	return window;
+}
+
+LPSTR UI::getClassName()
+{
+	return className;
+}
+
+void UI::createView(HINSTANCE instance, LPSTR title)
+{
+	view = CreateWindowEx(WS_EX_WINDOWEDGE, className, title, WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL, instance, NULL);
+}
+
+HWND UI::getView()
+{
+	return view;
+}
+
+LRESULT CALLBACK UI::Proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	return me->WndProc(hwnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK UI::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_COMMAND:
+		if ((HWND)lParam == generate.getHandle()) {
+			// generate
+		}
+		else if ((HWND)lParam == show.getHandle()) {
+
+		}
+		break;
+
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+
+	return 0;
+}
+
+Control::Control()
+{
+	type = (LPSTR)"STATIC";
+	title = (LPSTR)"default";
+	x = 0;
+	y = 0;
+	width = 0;
+	height = 0;
+}
+
+Control::Control(LPSTR type, LPSTR title, int x, int y, int width, int height)
+{
+	this->title = title;
+	this->type = type;
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+}
+
+void Control::setParams(LPSTR type, LPSTR title, int x, int y, int width, int height)
+{
+	this->title = title;
+	this->type = type;
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+}
+
+void Control::initialize(HWND handle, HINSTANCE instance)
+{
+	control = CreateWindowEx(0, type, title, WS_CHILD | WS_VISIBLE, x, y, width, height, handle, NULL, instance, NULL);
+}
+
+HWND Control::getHandle()
+{
+	return control;
+}
--- SVGgen/SVGgen/SVGgen.h
+++ SVGgen/SVGgen/SVGgen.h
@@ -1,2 +1,40 @@
 #pragma once
 #include <iostream>
+#include <windows.h>
+
+typedef enum Handles {
+
+} handles;
+
+class Control {
+public:
+	Control();
+	Control(LPSTR type, LPSTR title, int x, int y, int width, int height);
+	void setParams(LPSTR type, LPSTR title, int x, int y, int width, int height);
+	void initialize(HWND handle, HINSTANCE instance);
+	HWND getHandle();
+private:
+	HWND control;
+	LPSTR type, title;
+	int x, y, width, height;
+};
+
+class UI {
+public:
+	UI();
+	UI(HINSTANCE instance, LPSTR className, int width, int height);
+	static LRESULT CALLBACK Proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	WNDCLASSEX getWindow();
+	LPSTR getClassName();
+	void createView(HINSTANCE instance, LPSTR title);
+	HWND getView();
+private:
+	LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	LPSTR className;
+	WNDCLASSEX window;
+	int width, height;
+	HWND view; // g³óne okno widoku
+	Control generate, show; // uchwyty do guzików
+	static UI *me;
+};
+
